57f8ce4c51754d04dd9d4ad826f9a5071521da69
Hussain Arif <hussainarifkl@gmail.com>
2025-05-27 19:10:44 +0200

    Add another test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <p>Hello world</p>
+      <button data-testid="increment" onClick={() => setCount((c) => c + 1)}>
+        Increment
+      </button>
+      <p data-testid="count-value">Count: {count}</p> {/* Count: 1*/}
     </>
   );
 }
